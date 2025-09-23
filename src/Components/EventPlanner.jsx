0fe24ba7e7721b22f92f5e3a0b355ccfb212111e
Selwyn Paul Taloza <selwyntaloza@gmail.com>
2025-09-24 00:47:59 +0800

    first section "description" added

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import './EventPlanner.css'; // Import CSS file for styling
+import React from "react";
+import "./EventPlanner.css"; // Import CSS file for styling
 
 const EventPlanner = () => {
-    return (
-        <div className="event-planner-container">
-            <header>
-                <h1>Welcome to Event Planner</h1>
-            </header>
-            <section></section>
-            <section></section>
-            <section></section>
-            <section></section>
-            <section></section>
-        </div>
-    );
+  return (
+    <div className="event-planner-container">
+      <header>
+        <h1>Welcome to Event Planner</h1>
+      </header>
+      <section className="description">
+        <p>
+          From intimate dinners to grand celebrations, our Event Planner makes
+          organizing your special day seamless and stress-free. Focus on the
+          memories, and let us handle the details.
+        </p>
+        <button className="get-started-button">Get Started!</button>
+      </section>
+      <section></section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </div>
+  );
 };
 
 export default EventPlanner;
